f5557039ce294d2e28bbbf34e2e155f59895f078
Dawid Ciężarkiewicz <dpc@dpc.pw>
2017-08-08 23:05:27 -0700

    Misc `FastCDC` fixes

--- src/fastcdc.rs
+++ src/fastcdc.rs
@@ -1,7 +1,6 @@
 use super::{RollingHash, CDC};
 use std::default::Default;
-use std::cmp;
-use std::mem;
+use std::{cmp, mem};
 use {gear, Gear};
 
 fn get_masks(avg_size: usize, nc_level: usize, seed: u64) -> (u64, u64) {
@@ -30,44 +29,29 @@ fn get_masks(avg_size: usize, nc_level: usize, seed: u64) -> (u64, u64) {
 pub struct FastCDC {
     current_chunk_size: u64,
     gear: Gear,
-    mask_long: u64,
     mask_short: u64,
+    mask_long: u64,
+    ignore_size: u64,
+    min_size: u64,
+    avg_size: u64,
+    max_size: u64,
 }
 
 impl Default for FastCDC {
     fn default() -> Self {
-        let (mask_short, mask_long) = get_masks(1 << gear::CHUNK_BITS, 2, 0);
-        FastCDC {
-            current_chunk_size: 0,
-            gear: Gear::default(),
-            mask_short: mask_short,
-            mask_long: mask_long,
-        }
-    }
-}
-
-
-impl RollingHash for FastCDC {
-    type Digest = u64;
-
-    fn roll_byte(&mut self, b: u8) {
-        self.gear.roll_byte(b);
-    }
-
-    fn digest(&self) -> u64 {
-        self.gear.digest()
-    }
-
-    fn reset(&mut self) {
-        self.gear.reset();
-        self.current_chunk_size = 0;
+        FastCDC::new()
     }
 }
 
 impl FastCDC {
     /// Create new FastCDC engine with default chunking settings
     pub fn new() -> Self {
-        Default::default()
+        FastCDC::new_with_chunk_bits(gear::CHUNK_BITS)
+    }
+
+    fn reset(&mut self) {
+        self.gear.reset();
+        self.current_chunk_size = 0;
     }
 
     /// Create new `FastCDC` engine with custom chunking settings
@@ -76,62 +60,61 @@ impl FastCDC {
     /// the edge condition. `CHUNK_BITS` constant is the default.
     pub fn new_with_chunk_bits(chunk_bits: u32) -> Self {
         let (mask_short, mask_long) = get_masks(1 << chunk_bits, 2, 0);
-        Self {
-            current_chunk_size: 0,
-            gear: Gear::new_with_chunk_bits(chunk_bits),
-            mask_short: mask_short,
-            mask_long: mask_long,
-        }
-    }
-}
-
-impl CDC for FastCDC {
-    /// Find chunk edge using `FastCDC` defaults.
-    ///
-    /// See `RollingHash::find_chunk_edge_cond`.
-    fn find_chunk<'a>(&mut self, whole_buf: &'a [u8]) -> Option<(&'a [u8], &'a [u8])> {
-
+        let gear = Gear::new_with_chunk_bits(chunk_bits);
         const DIGEST_SIZE: usize = 64;
         debug_assert_eq!(
-            mem::size_of::<<Self as RollingHash>::Digest>() * 8,
+            mem::size_of::<<Gear as RollingHash>::Digest>() * 8,
             DIGEST_SIZE
         );
 
         const SPREAD_BITS: u32 = 3;
         const WINDOW_SIZE: usize = 64;
 
-        let min_mask = self.mask_short;
-        let max_mask = self.mask_long;
-
-        let min_size = (1 << (self.gear.chunk_bits - SPREAD_BITS + 1)) as u64;
+        let min_size = (1 << (gear.chunk_bits - SPREAD_BITS + 1)) as u64;
 
         let ignore_size = min_size - WINDOW_SIZE as u64;
-        let avg_size = (1 << self.gear.chunk_bits) as u64;
-        let max_size = (1 << (self.gear.chunk_bits + SPREAD_BITS)) as u64;
+        let avg_size = (1 << gear.chunk_bits) as u64;
+        let max_size = (1 << (gear.chunk_bits + SPREAD_BITS)) as u64;
 
+        Self {
+            current_chunk_size: 0,
+            gear: gear,
+            mask_short: mask_short,
+            mask_long: mask_long,
+            ignore_size: ignore_size,
+            min_size: min_size,
+            avg_size: avg_size,
+            max_size: max_size,
+        }
+    }
+}
+
+impl CDC for FastCDC {
+    /// Find chunk edge using `FastCDC` defaults.
+    fn find_chunk<'a>(&mut self, whole_buf: &'a [u8]) -> Option<(&'a [u8], &'a [u8])> {
         let mut buf = whole_buf;
 
-        debug_assert!(self.current_chunk_size < max_size);
+        debug_assert!(self.current_chunk_size < self.max_size);
 
         // ignore bytes that are not going to influence the digest
-        if self.current_chunk_size < ignore_size {
-            let skip_bytes = cmp::min(ignore_size - self.current_chunk_size, buf.len() as u64);
+        if self.current_chunk_size < self.ignore_size {
+            let skip_bytes = cmp::min(self.ignore_size - self.current_chunk_size, buf.len() as u64);
             self.current_chunk_size += skip_bytes;
             buf = &buf[skip_bytes as usize..];
         }
 
         // ignore edges in bytes that are smaller than min_size
-        if self.current_chunk_size < min_size {
-            let roll_bytes = cmp::min(min_size - self.current_chunk_size, buf.len() as u64);
+        if self.current_chunk_size < self.min_size {
+            let roll_bytes = cmp::min(self.min_size - self.current_chunk_size, buf.len() as u64);
             self.gear.roll(&buf[..roll_bytes as usize]);
             self.current_chunk_size += roll_bytes;
             buf = &buf[roll_bytes as usize..];
         }
 
         // roll through early bytes with smaller probability
-        if self.current_chunk_size < avg_size {
-            let roll_bytes = cmp::min(avg_size - self.current_chunk_size, buf.len() as u64);
-            let result = self.gear.find_chunk_mask(buf, min_mask);
+        if self.current_chunk_size < self.avg_size {
+            let roll_bytes = cmp::min(self.avg_size - self.current_chunk_size, buf.len() as u64);
+            let result = self.gear.find_chunk_mask(buf, self.mask_short);
 
             if let Some(result) = result {
                 self.reset();
@@ -143,9 +126,9 @@ impl CDC for FastCDC {
         }
 
         // roll through late bytes with higher probability
-        if self.current_chunk_size < max_size {
-            let roll_bytes = cmp::min(max_size - self.current_chunk_size, buf.len() as u64);
-            let result = self.gear.find_chunk_mask(buf, max_mask);
+        if self.current_chunk_size < self.max_size {
+            let roll_bytes = cmp::min(self.max_size - self.current_chunk_size, buf.len() as u64);
+            let result = self.gear.find_chunk_mask(buf, self.mask_long);
 
             if let Some(result) = result {
                 self.reset();
@@ -156,8 +139,8 @@ impl CDC for FastCDC {
             buf = &buf[roll_bytes as usize..];
         }
 
-        if self.current_chunk_size >= max_size {
-            debug_assert_eq!(self.current_chunk_size, max_size);
+        if self.current_chunk_size >= self.max_size {
+            debug_assert_eq!(self.current_chunk_size, self.max_size);
             let result = (&whole_buf[..whole_buf.len() - buf.len()], buf);
             self.reset();
             return Some(result);
@@ -172,30 +155,6 @@ impl CDC for FastCDC {
 
 #[cfg(test)]
 mod tests {
-    use super::{FastCDC, RollingHash};
-
-    #[test]
-    fn effective_window_size() {
-        let ones = vec![0x1; 1024];
-        let zeroes = vec![0x0; 1024];
-
-        let mut gear = FastCDC::new();
-        gear.roll(&ones);
-        let digest = gear.digest();
-
-        let mut gear = FastCDC::new();
-        gear.roll(&zeroes);
-
-        for (i, &b) in ones.iter().enumerate() {
-            gear.roll_byte(b);
-            if gear.digest() == digest {
-                assert_eq!(i, 63);
-                return;
-            }
-        }
-
-        panic!("matching digest not found");
-    }
 
     #[cfg(feature = "bench")]
     mod bench {
