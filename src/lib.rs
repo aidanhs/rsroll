2b8b459ed39b686bf9311a77a73e0978b99dca5f
Zachary Dremann <dremann@gmail.com>
2022-06-11 19:08:50 -0400

    Add general tests for an Engine

--- src/lib.rs
+++ src/lib.rs
@@ -63,3 +63,68 @@ pub trait Engine {
         None
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use nanorand::{Rng, WyRand};
+
+    fn rand_data(len: usize) -> Vec<u8> {
+        let mut data = vec![0; len];
+        let mut rng = WyRand::new_seed(0x01020304);
+        rng.fill_bytes(&mut data);
+        data
+    }
+
+    macro_rules! test_engine {
+        ($name:ident, $engine:ty) => {
+            mod $name {
+                use super::*;
+
+                #[test]
+                fn roll_byte_same_as_roll() {
+                    let mut engine1 = <$engine>::default();
+                    let mut engine2 = <$engine>::default();
+
+                    let data = rand_data(1024);
+                    for (i, &b) in data.iter().enumerate() {
+                        engine1.roll_byte(b);
+
+                        engine2.reset();
+                        engine2.roll(&data[..=i]);
+                        assert_eq!(engine1.digest(), engine2.digest());
+
+                        let mut engine3 = <$engine>::default();
+                        engine3.roll(&data[..=i]);
+                        assert_eq!(engine1.digest(), engine3.digest());
+                    }
+                }
+
+                #[test]
+                fn chunk_edge_correct_digest() {
+                    let mut engine1 = <$engine>::default();
+
+                    let data = rand_data(512 * 1024);
+                    let mut remaining = &data[..];
+                    while let Some((i, digest)) =
+                        engine1.find_chunk_edge_cond(remaining, |e| e.digest() & 0x0F == 0x0F)
+                    {
+                        let mut engine2 = <$engine>::default();
+                        engine2.roll(&remaining[..i]);
+                        assert_eq!(engine2.digest(), digest);
+
+                        remaining = &remaining[i..];
+                        engine2.reset();
+                        assert_eq!(engine2.digest(), engine1.digest());
+                    }
+                }
+            }
+        };
+    }
+
+    #[cfg(feature = "bup")]
+    test_engine!(bup, Bup);
+
+    #[cfg(feature = "gear")]
+    test_engine!(gear, Gear);
+}
